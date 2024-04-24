ecd4a59e765b1319e5d82a5235646047414370e0
neverstopcreating <karolina.uskure@inbox.lv>
2024-04-24 17:06:37 +0300

    Puzzle stitcher for png images

---
+++ src/pages/components/Canvas.tsx
@@ -0,0 +1,110 @@
+import PuzzlePiece, {loadPuzzlePiece} from "@/pages/components/PuzzlePiece";
+import {useEffect, useRef} from "react";
+
+
+function Canvas() {
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    useEffect(() => {
+        async function setupCanvas() {
+            const canvas = canvasRef.current;
+            if (!canvas) {
+                console.error("Failed to find the canvas element");
+                return;
+            }
+
+            const ctx = canvas.getContext('2d', {willReadFrequently: true});
+            if (!ctx) {
+                console.error("Failed to get canvas context");
+                return;
+            }
+
+            const loadingPuzzlePieces: Promise<PuzzlePiece>[] = [];
+            for (let i = 0; i < 256; i++) {
+                const url = `/peaces/peace-${i}.png`;
+                loadingPuzzlePieces.push(loadPuzzlePiece(url))
+            }
+
+            const puzzlePieces = await Promise.all(loadingPuzzlePieces);
+            assemblePuzzle(puzzlePieces, ctx);
+        }
+
+        //I am grouping the pieces by their sizes before matching by colors
+        function groupPuzzlePieces(pieces: PuzzlePiece[]): {
+            topLeftPiece: PuzzlePiece,
+            leftColumnPiecesGroup: Set<PuzzlePiece>,
+            otherPiecesGroup: Set<PuzzlePiece>
+        } {
+            let topLeftPiece: PuzzlePiece | undefined = undefined;
+            const leftColumnPiecesGroup: Set<PuzzlePiece> = new Set();
+            const otherPiecesGroup: Set<PuzzlePiece> = new Set();
+
+            pieces.forEach(piece => {
+                if (piece.image.width === 240 && piece.image.height === 135) {
+                    topLeftPiece = piece;
+                } else if (piece.image.width === 240 && piece.image.height === 136) {
+                    leftColumnPiecesGroup.add(piece);
+                } else {
+                    otherPiecesGroup.add(piece);
+                }
+            });
+
+            if (!topLeftPiece) {
+                throw new Error('Top left piece not found');
+            }
+
+            return {topLeftPiece, leftColumnPiecesGroup, otherPiecesGroup};
+        }
+
+        function assemblePuzzle(pieces: PuzzlePiece[], ctx: CanvasRenderingContext2D) {
+
+            const puzzleWidthInPieces = 16;
+            const puzzleHeightInPieces = 16;
+
+            const {topLeftPiece, leftColumnPiecesGroup, otherPiecesGroup} = groupPuzzlePieces(pieces);
+
+            // Put top left in its place
+            topLeftPiece.draw(ctx, 0, 0);
+
+            // Create arrays to hold matched ones for the top row and left column
+            const leftColumnPieces = [topLeftPiece];
+
+            let previousPiece = topLeftPiece;
+            for (let y = 1; y < puzzleHeightInPieces; y++) {
+                for (const pieceToCheck of leftColumnPiecesGroup) {
+                    if (previousPiece.matchWith(pieceToCheck, 'bottom')) {
+                        pieceToCheck.draw(ctx, 0, y);
+                        leftColumnPieces.push(pieceToCheck);
+                        previousPiece = pieceToCheck;
+                        leftColumnPiecesGroup.delete(pieceToCheck);
+                        break;
+                    }
+                }
+            }
+
+            for (let y = 0; y < puzzleHeightInPieces; y++) {
+                let previousPiece = leftColumnPieces[y];
+                for (let x = 1; x < puzzleWidthInPieces; x++) {
+                    for (const pieceToCheck of otherPiecesGroup) {
+                        if (previousPiece.matchWith(pieceToCheck, 'right')) {
+                            pieceToCheck.draw(ctx, x, y);
+                            previousPiece = pieceToCheck;
+                            otherPiecesGroup.delete(pieceToCheck);
+                            break;
+                        }
+
+                    }
+                }
+            }
+        }
+
+        setupCanvas();
+    }, []); // Empty array makes sure this effect runs only 1 time after the component mounts
+    return (
+        <div>
+            <h1>Puzzle stitcher</h1>
+            <canvas ref={canvasRef} width="3840" height="2160"></canvas>
+        </div>
+    );
+}
+
+export default Canvas;
---
+++ src/pages/components/PuzzlePiece.tsx
@@ -0,0 +1,127 @@
+//loading puzzle and extracting buffer data before matching and drawing visual for user
+export function loadPuzzlePiece(imageSrc: string): Promise<PuzzlePiece> {
+    return new Promise((resolve, reject) => {
+        const image = new Image();
+        image.src = imageSrc;
+        image.onload = () => {
+            const offScreenCanvas = document.createElement('canvas');
+            offScreenCanvas.width = image.width;
+            offScreenCanvas.height = image.height;
+            const ctx = offScreenCanvas.getContext('2d', {willReadFrequently: true})!;
+            ctx.drawImage(image, 0, 0);
+            const imageData = ctx.getImageData(0, 0, offScreenCanvas.width, offScreenCanvas.height);
+            const buffer = imageData.data;
+            resolve(new PuzzlePiece(image, buffer));
+        };
+        image.onerror = () => {
+            reject('Image could not be loaded');
+        };
+    })
+}
+
+export default class PuzzlePiece {
+    image: HTMLImageElement;
+    edgeData: { top: number[], right: number[], bottom: number[], left: number[] };
+
+    constructor(image: HTMLImageElement, buffer: Uint8ClampedArray) {
+        this.image = image;
+        this.edgeData = {top: [], right: [], bottom: [], left: []};
+        this.extractEdges(buffer);
+    }
+
+    //I am using only 2 sides in my alghoritm but it can be easily extended to 4 sides for future implementations
+    matchWith(otherPiece: PuzzlePiece, edgeToMatch: 'top' | 'bottom' | 'left' | 'right'): boolean {
+        const thisEdge = this.getEdgeData(edgeToMatch);
+        const otherEdge = otherPiece.getEdgeData(this.getOppositeEdge(edgeToMatch));
+
+        // Check length
+        if (thisEdge.length !== otherEdge.length) {
+            return false;
+        }
+
+        // Check pixel colors
+        for (let i = 0; i < thisEdge.length; i++) {
+            if (thisEdge[i] !== otherEdge[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    draw(ctx: CanvasRenderingContext2D, x: number, y: number) {
+        ctx.drawImage(this.image, x * 240, y * 135);
+    }
+
+    private extractEdges(buffer: Uint8ClampedArray) {
+        // Get pixel colors from sides
+        this.edgeData.top = this.extractEdgeData(buffer, 'top');
+        this.edgeData.right = this.extractEdgeData(buffer, 'right');
+        this.edgeData.bottom = this.extractEdgeData(buffer, 'bottom');
+        this.edgeData.left = this.extractEdgeData(buffer, 'left');
+    }
+
+    private extractEdgeData(buffer: Uint8ClampedArray, edge: 'top' | 'bottom' | 'left' | 'right'): number[] {
+        const width = this.image.width;
+        const height = this.image.height;
+        const colors: number[] = [];
+
+        switch (edge) {
+            case 'top':
+                for (let x = 0; x < width; x++) {
+                    const index = (x * 4); //RGBA 4 values
+                    const r = buffer[index];
+                    const g = buffer[index + 1];
+                    const b = buffer[index + 2];
+                    colors.push(Math.floor((r + g + b) / 3));
+                }
+                break;
+            case 'bottom':
+                for (let x = 0; x < width; x++) {
+                    const index = ((width * (height - 1) + x) * 4);
+                    const r = buffer[index];
+                    const g = buffer[index + 1];
+                    const b = buffer[index + 2];
+                    colors.push(Math.floor((r + g + b) / 3));
+                }
+                break;
+            case 'left':
+                for (let y = 0; y < height; y++) {
+                    const index = (y * width * 4); // Start of each row
+                    const r = buffer[index];
+                    const g = buffer[index + 1];
+                    const b = buffer[index + 2];
+                    colors.push(Math.floor((r + g + b) / 3));
+                }
+                break;
+            case 'right':
+                for (let y = 0; y < height; y++) {
+                    const index = ((y * width + width - 1) * 4); // Last pixel in each row
+                    const r = buffer[index];
+                    const g = buffer[index + 1];
+                    const b = buffer[index + 2];
+                    colors.push(Math.floor((r + g + b) / 3));
+                }
+                break;
+        }
+        return colors;
+    }
+
+    private getEdgeData(edge: 'top' | 'bottom' | 'left' | 'right'): number[] {
+        return this.edgeData[edge];
+    }
+
+    private getOppositeEdge(edge: 'top' | 'bottom' | 'left' | 'right'): 'top' | 'bottom' | 'left' | 'right' {
+        switch (edge) {
+            case 'top':
+                return 'bottom';
+            case 'bottom':
+                return 'top';
+            case 'left':
+                return 'right';
+            case 'right':
+                return 'left';
+            default:
+                throw new Error('Invalid edge');
+        }
+    }
+}
